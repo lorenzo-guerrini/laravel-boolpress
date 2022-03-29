92b18a4d9a2dbaa7c48420cdc7237136e1e4681c
lorenzo-guerrini <lorenzoguerrini8@live.it>
2022-03-29 17:58:13 +0200

    Creata pagina about

---
+++ resources/js/guest/pages/About.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="container">
+    <h1>About Us</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "About",
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
