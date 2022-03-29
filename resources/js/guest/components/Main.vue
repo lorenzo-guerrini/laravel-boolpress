9281c9e6c5e46137d102928d6a5eacb437066c1b
lorenzo-guerrini <lorenzoguerrini8@live.it>
2022-03-29 04:34:56 +0200

    Iniziato css

--- resources/js/guest/components/Main.vue
+++ resources/js/guest/components/Main.vue
@@ -1,8 +1,24 @@
 <template>
-  <div>
-    <div v-for="(post, index) in posts" :key="index">
-      <h2>{{ post.title }}</h2>
-      <p>{{ post.content }}</p>
+  <div class="container">
+    <div v-for="post in posts" :key="post.slug" class="post">
+      <img
+        v-if="post.image"
+        class="cover"
+        :src="`/storage/${post.image}`"
+        :alt="post.title"
+      />
+
+      <h2 class="title">{{ post.title }}</h2>
+
+      <div v-if="post.category" class="category">{{ post.category.name }}</div>
+
+      <ul class="tags">
+        <li class="tag" v-for="tag in post.tags" :key="tag.slug">
+          {{ tag.name }}
+        </li>
+      </ul>
+
+      <p class="content">{{ post.content }}</p>
     </div>
   </div>
 </template>
@@ -23,5 +39,44 @@ export default {
 };
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+.container {
+  display: flex;
+  flex-wrap: wrap;
+}
+
+.post {
+  width: 200px;
+  background: lightblue;
+
+  .title {
+    padding: 10px;
+  }
+
+  .cover {
+    width: 100%;
+  }
+
+  .category {
+    display: inline-block;
+    background: blue;
+    padding: 10px;
+    border-radius: 5px;
+    text-transform: uppercase;
+    margin: 0 10px;
+  }
+
+  .tags {
+    margin: 0 5px;
+    .tag {
+      background: wheat;
+      padding: 0px 10px;
+      border-radius: 5px;
+    }
+  }
+
+  .content {
+    padding: 20px 5px;
+  }
+}
 </style>
